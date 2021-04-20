4651d273222ef43bfff6db68a069c63cc8fe166f
Amirsam Bahador <Java2Oracle@yahoo.com>
2021-04-20 18:12:19 +0430

    Update container.js

--- web/assets/j2os-js/container.js
+++ web/assets/j2os-js/container.js
@@ -255,8 +255,7 @@ class UIManager {
          */
         let html = "";
         for (const jsonObject of JSON.parse(jsonArray)) {
-            html += "<tr " + onEvent + "='" + callback + "(" + JSON.stringify(jsonObject) + ")'>";
-            alert(html)
+            html += "<tr class='clickable-row' " + onEvent + "='" + callback + "(" + JSON.stringify(jsonObject) + ")'>";
             for (const columnName of columnArray) {
                 html += "<td>";
                 html += jsonObject[columnName];
